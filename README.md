5ab84eef174185821a1f46c3c047a8f614d1ff99
likun <likun01@vcredit.com>
2017-11-08 11:02:05 +0800

    啊啊啊

---
+++ README.md
@@ -0,0 +1,18 @@
+# **AndroidEventBus**
+    android 异常捕获
+
+### 1.用法
+使用前，对于Android Studio的用户，可以选择添加:
+    
+	compile 'com.github.ashLikun.AppCrashDispose:0.0.1'
+
+### 2.API
+     CaocConfig.Builder.create()
+                    .enabled(true) //default: true
+                    .showRestartButton(true) //default: true
+                    .trackActivities(true) //default: false
+                    .minTimeBetweenCrashesMs(2000) //default: 3000
+                    .eventListener()
+                    .apply();
+
+
---
+++ app/src/main/java/com/ashlikun/appcrash/simple/MyApp.java
@@ -0,0 +1,26 @@
+package com.ashlikun.appcrash.simple;
+
+import android.app.Application;
+
+import com.ashlikun.appcrash.config.CaocConfig;
+
+/**
+ * 作者　　: 李坤
+ * 创建时间: 2017/11/7　17:43
+ * 邮箱　　：496546144@qq.com
+ * <p>
+ * 功能介绍：
+ */
+
+public class MyApp extends Application{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        CaocConfig.Builder.create()
+                .enabled(true) //default: true
+                .showRestartButton(true) //default: true
+                .trackActivities(true) //default: false
+                .minTimeBetweenCrashesMs(2000) //default: 3000
+                .apply();
+    }
+}
